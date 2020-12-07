537786a6c9f17f24a136819c1b61eab37b879e32
seda-karadeniz <sedayo98@gmail.com>
2020-12-07 15:07:59 +0100

    completed fizzbuzz

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 document.documentElement.classList.add('js-enabled');
 
-document.querySelector('#fizzbuzz').insertAdjacentHTML('beforeend', '<li class="fizzbuzz">FI<i>zz</i>BU<i>zz</i></li>');
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 ===0 ) {
+           document.querySelector('#fizzbuzz').insertAdjacentHTML('beforeend', '<li class="fizzbuzz">FI<i>zz</i>BU<i>zz</i></li>');
+    }
+    else if (i % 3 ===0) {
+        document.querySelector('#fizzbuzz').insertAdjacentHTML('beforeend', '<li class="fizz">FI<i>zz</i></li>');
+
+    }
+    else if (i % 5 ===0) {
+        document.querySelector('#fizzbuzz').insertAdjacentHTML('beforeend', '<li class="buzz">BU<i>zz</i></li>');
+
+    }
+    else{
+        document.querySelector('#fizzbuzz').insertAdjacentHTML('beforeend', `<li>${i}</li>`);
+   
+    }
+    
+}
